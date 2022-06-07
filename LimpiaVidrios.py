4f07f2c7c6c15ea50cf322b3f01bf257c1402525
LuisOlalla <75482079+LuisOlalla@users.noreply.github.com>
2022-06-07 16:21:17 -0500

    Agente Limpia Vidrios

---
+++ LimpiaVidrios.py
@@ -0,0 +1,31 @@
+#PROYECTO UNIDAD 1 AGENTE ROBOT LIMPIAVIDRIOS EN UN EDIFICIO DE LA ESPE
+#Integrantes: Carlos Lucio , Luis Olalla
+#Curso: 6 to A
+#Asignatura: Inteligencia Artificial
+
+#Programar el agente
+
+#Definimos funcíon del Robot Limpia Vidrios
+def LimpiaVidrios():
+    """
+    Funcion del robot que va a contener todo el control del flujo del programa
+    mediante bloques de codigo de cada locación.
+    """  
+    #Colocamos el estado objetivo de nuestro agente.
+    objetivo = {'V1':'0','V2':'0','V3':'0','V4':'0','V5':'0','V6':'0','V7':'0'}
+    esfuerzo = 0 #Esto es el esfuerzo que realiza el robot al cambiar de estado.
+    #Estados del robot LimpiaVidrios
+    print("ESTADOS DEL ROBOT LIMPIA VIDRIOS: ")
+    print("Ventana Sucia = 1") #Si esta en 1 significa que hay ventanas sin limpiar.
+    print("Ventana Limpia = 0 ") #Si esta en 0 es porque no hjay ventanas sucias.
+    #Las locaciones o escenarios de nuestro robot son:
+    print("LOCACIONES DEL ROBOT LIMPIA VIDRIOS:  ")
+    print("Ventana 1 = V1\nVentana 2 = V2\nVentana 3 = V3\nVentana 4 = V4")
+    print("Ventana 5 = V5\nVentana 6 = V6\nVentana 7 = V7")
+    
+      
+    
+    
+    
+
+LimpiaVidrios()

2a669911a6e81f703a1abec3a81d30d8c25d911e
Celucio <75482093+Celucio@users.noreply.github.com>
2022-06-09 15:00:34 -0500

    Creación del método busqueda con su constructor y el método añadir nodo

---
+++ AlgoritmoGPS.py
@@ -0,0 +1,56 @@
+'''
+Implementación del algoritmo de busqueda BFS para generar la simulación de un GPS
+La orientación se basa en la planificación de un viaje para encontrar lugares cercanos 
+de interés por medio de un método de busqueda. 
+'''
+#Importamos la clase queue para la generación de Colas
+from queue import Queue
+
+'''
+Generar la clase grafo que permite instanciar el objeto y definir
+los nodos a considerar para obtener la búsqueda deseada
+'''
+class Grafo():
+    
+    def __init__(self, numero_de_nodos,diccionario,dirigido=True):
+        '''
+        Generar el constructor, este método permite instanciar el objeto creado
+            Parámetros:
+                numero_de_nodos : int
+                    Determina el rango que tendra el grafo
+                diccionario : set()
+                    Genera una lista de los nodos que van a ser visitados
+                dirigido : boolean
+                    Determina si el grafo es dirigido o no
+            Retorna:
+                Nada
+        '''
+        #Asignamos valores al número de nodos mediante el parámetro que se recibio
+        self.m_numero_nodos = numero_de_nodos
+        #Se genera el rango de nodos en base a m_numero_nodos
+        self.m_nodos = range(self.m_numero_nodos)
+        #Determinar si el grafo es dirigido o no dirigido
+        self.m_dirigido = dirigido
+        #Se genera un diccionario con los nodos que se van a visitar
+        self.m_locaciones = diccionario
+        #Se genera una lista de los nodos que se van a visitar
+        self.m_lista_adyacencia = {nodo: set() for nodo in self.m_nodos}
+    
+    def añadir_locaciones(self,nodo1,nodo2,peso=1):
+        '''
+        Generar el método para añadir nodos al grafo
+        Parámetros:
+            nodo1 : int
+                Nodo que se va a añadir
+            nodo2 : int
+                Nodo que se va a añadir
+            peso : int
+                Peso que se le asigna al nodo
+        Retorna:
+            Nada
+        '''
+        #Se añade el nodo 1 al nodo 2
+        self.m_lista_adyacencia[nodo1].add(nodo2)
+        #Se añade el nodo 2 al nodo 1
+        if not self.m_dirigido:
+            self.m_lista_adyacencia[nodo2].add(nodo1)

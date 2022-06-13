c40f6dcc67b0e21f8ffc5a776cbe1f9de476106a
LuisOlalla <75482079+LuisOlalla@users.noreply.github.com>
2022-06-13 17:46:45 -0500

    Correccion

--- LimpiaVidrios1.py
+++ LimpiaVidrios1.py
@@ -8,6 +8,9 @@
 # Definimos funcíon del Robot Limpia Vidrios
 
 
+from cmath import e
+
+
 def LimpiaVidrios():
     """
     Funcion del robot que va a contener todo el control del flujo del programa
@@ -16,33 +19,43 @@ def LimpiaVidrios():
     # Colocamos el estado objetivo de nuestro agente.
     objetivo = {'Ventana 1': '0', 'Ventana 2': '0', 'Ventana 3': '0',
                 'Ventana 4': '0', 'Ventana 5': '0', 'Ventana 6': '0', 'Ventana 7': '0'}
-    locaciones ={'Ventana 1': '0', 'Ventana 2': '0', 'Ventana 3': '0',
+    locaciones = {'Ventana 1': '0', 'Ventana 2': '0', 'Ventana 3': '0',
                 'Ventana 4': '0', 'Ventana 5': '0', 'Ventana 6': '0', 'Ventana 7': '0'}
     costo = 0  # Esto es el esfuerzo que realiza el robot al cambiar de estado.
 
+
+    
+
     for llave in objetivo.keys():
+        
         
         print('Locación: ', llave)
 
         try:
-                estado_localidad = int(input('¿La ventana esta sucia?\n0 = No \n1 = Si\n'))
-                while str(estado_localidad) <'0' or str(estado_localidad) >'1':
-                    print("Ingrese valores numéricos de 0 a 1")
-                    estado_localidad = int(input('¿La ventana esta sucia?\n0 = No \n1 = Si\n'))
-                
-                if(estado_localidad == 1):
-                    print('Limpiando la ventana...')
-                    print('La ventana se ha limpiado correctamente') 
-                    costo+=1
-                    locaciones[llave] = estado_localidad   
-                    print('El  Costo actual es: ',str(costo))             
-                else:
-                    print('La ventana esta limpia ')
-                    print(' El Costo actual es: ',str(costo))
-                   
-        except ValueError:
-            print("Ponga solo numeros del 0 a 1")
-            estado_localidad = int(input('¿La ventana esta sucia?\n0 = No \n1 = Si\n'))
+                while True:
+                    
+                    estado_localidad = str(input('¿La ventana esta sucia?\n0 = No \n1 = Si\n'))
+                    if (estado_localidad !='0' and estado_localidad !='1'):
+                        print("Ingrese valores numéricos de 0 a 1")
+                        
+                    
+                    elif(estado_localidad == '1'):
+                        print('Limpiando la ventana...')
+                        print('La ventana se ha limpiado correctamente') 
+                        costo+=1
+                        locaciones[llave] = estado_localidad   
+                        print('El  Costo actual es: ',str(costo)) 
+                        break            
+                    elif(estado_localidad =='0'):
+
+                        print('La ventana esta limpia ')
+                        print(' El Costo actual es: ',str(costo))
+                        break
+        except Exception as e:
+
+
+            print(e)
+            
     print(str(locaciones))
     print(str(objetivo))
 

90e72ec8c1b5d0d8453eff01ea677688c19c0124
LuisOlalla <75482079+LuisOlalla@users.noreply.github.com>
2022-06-14 23:05:59 -0500

    fsdfsdf

--- LimpiaVidrios1_Unittest.py
+++ LimpiaVidrios1_Unittest.py
@@ -11,7 +11,7 @@ class TestLimpiaVidrios(unittest.TestCase):
         
         self.assertTrue(LimpiaVidrios,{'Ventana 1': '0', 'Ventana 2': '0', 'Ventana 3': '0', 'Ventana 4': '0', 'Ventana 5': '0', 'Ventana 6': '0', 'Ventana 7': '1'})
         
-        self.assertNotEqual(LimpiaVidrios,{'Ventana 1': '0', 'Ventana 2': '0', 'Ventana 3': '0', 'Ventana 4': '0', 'Ventana 5': '0', 'Ventana 6': '0', 'Ventana 7': '1'})
+        self.assertNotEqual(LimpiaVidrios,{'Ventana 1': '0', 'Ventana 2': '0', 'Ventana 3': '0', 'Ventana 4': '0', 'Ventana 5': '0', 'Ventana 6': '1', 'Ventana 7': '1'})
         
         
 
